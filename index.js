647192bb8fe3b2c82b81771234837e34e338e02e
Aadam Mazema <maze.aadam@gmail.com>
2024-09-05 16:36:37 +0200

    Update JavaScript to fetch cryptocurrency data
    Added a new fetch request to CoinGecko API to retrieve Ethereum data and populated div#crypto-top with the cryptocurrency’s image and name. Added <p> elements to div#crypto displaying the current price, 24h high, and 24h low and improved error handling for the CoinGecko API request and logged errors.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 // Fetch a random landscape nature photo from Unsplash API
 fetch(
   "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
@@ -9,10 +8,31 @@ fetch(
 
     // Set the fetched image as the background of the body and update author info
     document.body.style.backgroundImage = `url(${data.urls.regular})`;
-    document.getElementById("author").textContent = `By: ${data.user.name}`; 
+    document.getElementById("author").textContent = `By: ${data.user.name}`;
   })
 
-  // Default image fallback incase of error 
+  // Default image fallback incase of error
   .catch((err) => {
     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`;
   });
+
+// Fetch cryptocurrency data for Ethereum from CoinGecko API
+fetch("https://api.coingecko.com/api/v3/coins/ethereum")
+  .then((res) => {
+    if (!res.ok) {
+      throw Error("Something went wrong"); // Check for HTTP errors
+    }
+    return res.json();
+  })
+  .then((data) => {
+    // Update the crypto section with the retrieved data
+    document.getElementById("crypto-top").innerHTML = `
+    <img src=${data.image.small} />
+    <span>${data.name}</span>
+`;
+    document.getElementById("crypto").innerHTML += `
+<p>🎯: R${data.market_data.current_price.zar}</p>
+<p>👆: R${data.market_data.high_24h.zar}</p>
+<p>👇: R${data.market_data.low_24h.zar}</p>`;
+  })
+  .catch((err) => console.error(err)); // Log errors if the fetch fails
